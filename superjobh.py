3e80e5c9d0aa71dff7b329f2884aee8d6aec7025
igornast <grezle00@gmail.com>
2023-06-23 08:47:54 +0300

    Отредактирован класс SuperJobApi

--- superjobh.py
+++ superjobh.py
@@ -1,64 +1,70 @@
-from configparser import ParsingError
+import os
+from datetime import datetime
 import requests
 from abstract import JobAPI
-from pprint import pprint
 
 
 class SuperJobApi(JobAPI):
-    """ Класс для подключения к API superjob.ru"""
-    url = "https://api.superjob.ru/2.0/vacancies/"
+    """
+    Класс для работы с API SuperJob.
+    """
+    sj_api_secret_key = os.getenv('SJ_API_SECRET_KEY')
 
-    def __init__(self, keyword):
-        self.params = {
-            "text": keyword,
-            "area": False,
-            "per_page": 100,
-            "page": None,
-        }
-        self.headers = {
-                        "X-Api-App-Id": "v3.r.137614145.cb26bfdfa58f9da553a2e2914a14330a40f30d5e.c39d10f2ffd5e1eae7f5da48af569f7b7a09b985"
-        }
+    def __init__(self, keywords) -> None:
+        self.__keywords = keywords
+        self.__base_url = 'https://api.superjob.ru/2.0/vacancies/'
+        self.__headers = {'X-Api-App-Id': self.sj_api_secret_key}
         self.vacancies = []
 
-    def get_request(self):
-        """Метод получения вакансий"""
-        response = requests.get(self.url, headers=self.headers, params=self.params)
-        if response.status_code != 200:
-            raise ParsingError("Ошибка получения вакансий")
-        return response.json()["objects"]
-
-    def get_form_vacancies(self):
-        """Метод форматирования вакансий"""
-        form_vacancies = []
-
-        for vacancy in self.vacancies:
-            form_vacancy = {
-                "employer": vacancy["firm_name"],
-                "title": vacancy["profession"],
-                "url": vacancy["link"],
-                "api": "SuperJob",
-                "salary_from": vacancy["payment_from"] if vacancy["payment_from"] and vacancy["payment_from"] != 0 else None,
-                "salary_to": vacancy["payment_to"] if vacancy["payment_to"] and vacancy["payment_to"] != 0 else None,
-            }
-            form_vacancies.append(form_vacancy)
-
-        return form_vacancies
-
-    def get_vacancies(self, pages_count=4):
-        self.vacancies = []   # Очищаем список вакансий
-        for page in range(pages_count):
-            page_vacancies = []
-            self.params["page"] = page
-            print(f"({self.__class__.__name__}) Парсинг страницы {page} -", end=" ")
-
-            try:
-                page_vacancies = self.get_request()
-            except ParsingError as error:
-                print(error)
+    def get_vacancies(self) -> None:
+        """
+        Функция возвращает все вакансии по параметрам поиска.
+        """
+        params = {'keyword': self.__keywords, 'page': 0, 'count': 100}
+        vacancies_tmp = []
+        while True:
+            response = requests.get(self.__base_url, params=params, headers=self.__headers)
+            if response.status_code == 200:
+                print(f'{self.__class__.__name__} загрузкака страницы {params["page"]}')
+                data = response.json()
+                vacancies_tmp.extend(data['objects'])
+                more_results = data['more']
+                if not more_results:
+                    break
+                params['page'] += 1
             else:
-                self.vacancies.extend(page_vacancies)
-                print(f"Загружено вакансий: {len(page_vacancies)}")
+                print('Ошибка при получении списка вакансий с API SuperJob.ru:', response.text)
+        filtered_vacancies = self.__filter_vacancy(vacancies_tmp)
+        self.vacancies.extend(filtered_vacancies)
 
-            if len(page_vacancies) == 0:
-                break
+    @staticmethod
+    def __filter_vacancy(vacancy_data: list) -> list:
+        """
+        Функция извлекает и конвертирует данные о вакансиях.
+        :param vacancy_data:
+        :return: vacancy
+        """
+        vacancies = []
+        for vacancy in vacancy_data:
+            if not vacancy["is_closed"]:
+                datetime_obj = datetime.fromtimestamp(vacancy['date_published'])
+                formatted_date = datetime_obj.strftime("%Y.%m.%d %H:%M:%S")
+                payment_from = vacancy['payment_from'] if vacancy['payment_from'] is not None else 0
+                payment_to = vacancy['payment_to'] if vacancy['payment_to'] is not None else 0
+                processed_vacancy = {
+                    'platform': "SuperJob",
+                    "id": vacancy["id"],
+                    'title': vacancy['profession'],
+                    'company': vacancy['firm_name'],
+                    'url': vacancy['link'],
+                    'area': vacancy['town']['title'],
+                    'address': vacancy['address'],
+                    'candidat': vacancy['candidat'],
+                    'vacancyRichText': vacancy['vacancyRichText'],
+                    'date_published': formatted_date,
+                    'payment': {'from': payment_from, 'to': payment_to, 'currency': vacancy["currency"]}
+                }
+                vacancies.append(processed_vacancy)
+
+        return vacancies
 
