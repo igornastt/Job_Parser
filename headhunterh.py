bc8ff5ee1280ad734ab14e36efa845c944a1109e
igornast <grezle00@gmail.com>
2023-06-23 08:47:26 +0300

    Отредактирован класс HeadHunterAPI

--- headhunterh.py
+++ headhunterh.py
@@ -1,69 +1,74 @@
-from configparser import ParsingError
+from datetime import datetime
 import requests
 from abstract import JobAPI
-from pprint import pprint
 
 
 class HeadHunterAPI(JobAPI):
-    """ Класс для подключения к API hh.ru"""
-    url = "https://api.hh.ru/vacancies"
-
-    def __init__(self, keyword):
-        self.params = {
-            "text": keyword,
-            "area": False,
-            "per_page": 100,
-            "page": None,
-        }
-        self.headers = {"User-Agent": "MyImportantApp 1.0"}
+    """
+    Класс для работы с API HeadHunter.
+    """
+    def __init__(self, keywords) -> None:
+        self.__keywords = keywords
+        self.__base_url = "https://api.hh.ru/vacancies"
         self.vacancies = []
 
-    def get_request(self):
-        """Метод получения вакансий"""
-        response = requests.get(self.url, headers=self.headers, params=self.params)
-        if response.status_code != 200:
-            raise ParsingError("Ошибка получения вакансий")
-        return response.json()["items"]
+    def get_vacancies(self) -> None:
+        """
+        Функция возвращает вакансии по параметрам поиска.
+        """
+        vacancies_tmp = []
+        page = 0
+        total_pages = 1
 
-    def get_form_vacancies(self):
-        """Метод форматирования вакансий"""
-        form_vacancies = []
-
-        for vacancy in self.vacancies:
-            form_vacancy = {
-                "url": vacancy["alternate_url"],
-                "title": vacancy["name"],
-                "employer": vacancy["employer"]["name"],
-                "api": "HeadHunter",
-            }
-            salary = vacancy["salary"]
-            if salary:
-                form_vacancy["salary_to"] = salary["to"]
-                form_vacancy["salary_from"] = salary["from"]
-                form_vacancy["currency"] = salary["currency"]
-
+        while page < total_pages:
+            params = {'text': self.__keywords, 'page': page, 'per_page': 100}
+            response = requests.get(self.__base_url, params=params)
+            if response.status_code == 200:
+                print(f'{self.__class__.__name__} загрузкака страницы {page}')
+                data = response.json()
+                vacancies_tmp.extend(data["items"])
+                total_pages = data['pages']
+                page += 1
             else:
-                form_vacancy["salary_to"] = None
-                form_vacancy["salary_from"] = None
-                form_vacancy["currency"] = None
-            form_vacancies.append(form_vacancy)
+                print('Ошибка при получении списка вакансий с HeadHunter.ru:', response.text)
+            filtered_vacancies = self.__filter_vacancy(vacancies_tmp)
+            self.vacancies.extend(filtered_vacancies)
 
-        return form_vacancies
-
-    def get_vacancies(self, pages_count=4):
-        self.vacancies = []   # Очищаем список вакансий
-        for page in range(pages_count):
-            page_vacancies = []
-            self.params["page"] = page
-            print(f"({self.__class__.__name__}) Парсинг страницы {page} -", end=" ")
-
-            try:
-                page_vacancies = self.get_request()
-            except ParsingError as error:
-                print(error)
-            else:
-                self.vacancies.extend(page_vacancies)
-                print(f"Загружено вакансий: {len(page_vacancies)}")
-
-            if len(page_vacancies) == 0:
-                break
+    @staticmethod
+    def __filter_vacancy(vacancy_data: list) -> list:
+        """
+        Функция извлекает и конвертирует данные о вакансиях.
+        :param vacancy_data:
+        :return: vacancy
+        """
+        vacancies = []
+        for vacancy in vacancy_data:
+            if vacancy['type']['id'] == 'open':
+                address = vacancy['address']
+                address_raw = address['raw'] if address else None
+                salary = vacancy['salary']
+                if salary:
+                    salary_from = salary['from'] if salary['from'] is not None else 0
+                    salary_to = salary['to'] if salary['to'] is not None else 0
+                    currency = salary['currency'] if salary['currency'] is not None else "RUR"
+                else:
+                    salary_from = 0
+                    salary_to = 0
+                    currency = "RUR"
+                datetime_obj = datetime.strptime(vacancy['published_at'], "%Y-%m-%dT%H:%M:%S%z")
+                formatted_date = datetime_obj.strftime("%Y.%m.%d %H:%M:%S")
+                processed_vacancy = {
+                    'platform': "HeadHunter",
+                    'id': vacancy["id"],
+                    'title': vacancy['name'],
+                    'company': vacancy['employer']['name'],
+                    'url': vacancy['alternate_url'],
+                    'area': vacancy['area']['name'],
+                    'address': address_raw,
+                    'candidat': vacancy['snippet']['requirement'],
+                    'vacancyRichText': vacancy['snippet']['responsibility'],
+                    'date_published': formatted_date,
+                    'payment': {'from': salary_from, 'to': salary_to, 'currency': currency}
+                }
+                vacancies.append(processed_vacancy)
+        return vacancies
